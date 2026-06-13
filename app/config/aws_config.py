0146159d6bb58a8e37b64525d0e29ba414ebec0d
mohak007 <51796190+mohak007@users.noreply.github.com>
2026-06-13 16:45:16 +0530

    Set default AWS region to 'us-east-1'

--- app/config/aws_config.py
+++ app/config/aws_config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION = os.getenv("AWS_REGION")
+AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
 
 #ec2_client = boto3.client(
 #"ec2",
